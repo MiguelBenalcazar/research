1c1dac049641359f87981a310acd27b7273d04a0
Miguel Benalcazar <43470141+MiguelBenalcazar@users.noreply.github.com>
2025-05-15 15:05:14 +0800

    Add files via upload

---
+++ DETIC_DETR/detic_test.py
@@ -0,0 +1,36 @@
+from transformers import AutoImageProcessor, DeformableDetrForObjectDetection
+import torch
+from PIL import Image
+import requests
+
+
+
+
+def main():
+    url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+    image = Image.open(requests.get(url, stream=True).raw)
+
+    processor = AutoImageProcessor.from_pretrained("facebook/deformable-detr-detic")
+    model = DeformableDetrForObjectDetection.from_pretrained("facebook/deformable-detr-detic")
+
+    inputs = processor(images=image, return_tensors="pt")
+    image.show()
+    outputs = model(**inputs)
+
+    # convert outputs (bounding boxes and class logits) to COCO API
+    # let's only keep detections with score > 0.7
+    target_sizes = torch.tensor([image.size[::-1]])
+    results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.7)[0]
+
+    for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+        box = [round(i, 2) for i in box.tolist()]
+        print(
+            f"Detected {model.config.id2label[label.item()]} with confidence "
+            f"{round(score.item(), 3)} at location {box}"
+        )
+
+
+
+if __name__ == "__main__":
+    main()
+    
